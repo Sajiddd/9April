dff2322563a4721a9381758acdd4aead21239a33
Sajid <sajidnadaf717@gmail.com>
2023-04-09 03:57:39 +0530

    new project april 9

---
+++ ActiTime/src/test/java/p1/Demo1.java
@@ -0,0 +1,13 @@
+package p1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void testA()
+	{
+		Reporter.log("Test Demo1 ",true);
+	}
+
+}
